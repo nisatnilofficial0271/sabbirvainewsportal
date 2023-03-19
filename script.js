61fcf81c425da98184c34321f3c8d39d70d3cb88
nisatnilofficial0271 <107569069+nisatnilofficial0271@users.noreply.github.com>
2023-03-19 16:21:55 +0600

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-const toggleButton = document.querySelector('.toggle');
-
-const menu = document.querySelector('.menu');
-
-toggleButton.addEventListener('click', () => {
-
-	menu.classList.toggle('active');	toggleButton.classList.toggle('active');
-
-});
